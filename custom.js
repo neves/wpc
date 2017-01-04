436b58a9f593d9a2c4a2cecdb60b20a62817bd5b
Marcos Neves <marcos.neves@gmail.com>
2017-01-04 15:30:32 -0200

    support custom config that returns a function

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+// Accepts a custom configuration inside project
 const fs = require('fs')
 const resolve = require('path').resolve
 
@@ -5,6 +6,7 @@ module.exports = ({context, env}) => {
   var configFileName = env.config || 'wpc.conf.js'
   var custom = resolve(context, configFileName)
   if (fs.existsSync(custom)) {
-    return require(custom)
+    // instead of just use: return require(custom)
+    return [require(custom)] // hack to support a function (myself) that returns a function (result of require)
   }
 }
