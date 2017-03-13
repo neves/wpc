0f884dee453b61c17c0b49af1340f492fb03383b
Marcos Neves <marcos.neves@gmail.com>
2017-03-13 17:00:29 -0300

    Upgrade packages

--- tests/css/run.sh
+++ tests/css/run.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`
 rm -rf ../dist && \
-webpack --config webpackfile.js entry.js ../dist/bundle.js --hide-modules --progress && \
+webpack --config webpackfile.js entry.js '../dist/[name].js' --hide-modules --progress && \
 cat ../dist/main.css
--- tests/css/webpackfile.js
+++ tests/css/webpackfile.js
@@ -1,6 +1,8 @@
 const merge = require('../../merge')
 
 module.exports = merge(
+  require('../../env'),
   require('../../css'),
+  require('../../vendor'),
   require('../../delete-properties')
 )
